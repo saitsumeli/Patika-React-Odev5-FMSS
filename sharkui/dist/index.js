1d56cb2d8f4f3df1f5916907e2651be1930d7ea5
saitsumeli <saitsumeli@gmail.com>
2023-04-18 19:00:13 +0300

    first commit

---
+++ sharkui/dist/index.js
@@ -0,0 +1,15 @@
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var React = _interopDefault(require('react'));
+
+var styles = {"primary":"_styles-module__primary__3ljpl","dashed":"_styles-module__dashed__27xYC","text":"_styles-module__text__5bXm4","link":"_styles-module__link__71S8l"};
+
+var ExampleComponent = function ExampleComponent(_ref) {
+  var text = _ref.text;
+  return /*#__PURE__*/React.createElement("div", {
+    className: styles.test
+  }, "Example Component: ", text);
+};
+
+exports.ExampleComponent = ExampleComponent;
+//# sourceMappingURL=index.js.map
---
+++ sharkui/dist/index.modern.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+var styles = {"primary":"_styles-module__primary__3ljpl","dashed":"_styles-module__dashed__27xYC","text":"_styles-module__text__5bXm4","link":"_styles-module__link__71S8l"};
+
+const ExampleComponent = ({
+  text
+}) => {
+  return /*#__PURE__*/React.createElement("div", {
+    className: styles.test
+  }, "Example Component: ", text);
+};
+
+export { ExampleComponent };
+//# sourceMappingURL=index.modern.js.map
